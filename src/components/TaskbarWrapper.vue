e6f599301d21580c3bd14a5f2c79711068435b2f
Aleksa Nesic <anesic9@hotmail.rs>
2025-11-21 19:52:46 +0100

    Revert to using unified Taskbar component for both mobile and desktop

--- src/components/TaskbarWrapper.vue
+++ src/components/TaskbarWrapper.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts">
-import { useBreakpoints, breakpointsTailwind } from '@vueuse/core'
 import Taskbar from './Taskbar.vue'
-import TaskbarMobile from './TaskbarMobile.vue'
-
-const breakpoints = useBreakpoints(breakpointsTailwind)
-const isMobile = breakpoints.smaller('sm')
 </script>
 
 <template>
-  <TaskbarMobile v-if="isMobile" />
-  <Taskbar v-else />
+  <Taskbar />
 </template>
